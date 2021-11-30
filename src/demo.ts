940155736acc358668b51e9777b8b8e374f1ba34
Peter Zhang <peter.zhang.henan@gmail.com>
2021-11-30 16:49:46 +0800

    增加一些学习笔记

--- src/demo.ts
+++ src/demo.ts
@@ -8,6 +8,7 @@ type someType = string | number
 const boolArray: boolean[] = [false, true]
 
 const someArray: someType[] = [1, '2'] // 这样是可以的
+const simple: (foo: number) => string = foo => foo.toString()
 
-
+// 类型断言
 export default num
